7bdd8c3e4f5a2bd37313ae3642d1628bd6cc1fe0
Tran <tranvanhoc.tb@gmail.com>
2022-09-04 23:05:33 +0900

    Added AdminLTE3 template resource

---
+++ src/resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
+</div>
+<!-- ./wrapper -->
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE -->
+<script src="{{ asset('adminlte3/dist/js/adminlte.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script>
+
+@yield('scripts')
